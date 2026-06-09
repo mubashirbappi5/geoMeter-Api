68a7e5cf967df6aa0d4e0b72db0f30eeadd4c019
mubashirbappi <mubashirbappi@gmail.com>
2026-06-09 22:29:57 +0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+// Connect to Database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.json({ message: 'GeoMeter API is running successfully' });
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
